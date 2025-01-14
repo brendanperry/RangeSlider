0fef521f4e85323aa8e6600bd556cef97cc20dd5
Brendan Perry <brendan@brendanperry.me>
2025-01-13 22:22:25 -0500

    Adding source code

---
+++ Sources/RangeSlider/RangeSliderUnit.swift
@@ -0,0 +1,13 @@
+//
+//  RangeSliderUnit.swift
+//  RangeSlider
+//
+//  Created by Brendan Perry on 1/13/25.
+//
+
+protocol RangeSliderUnit {
+    func minValue() -> Double
+    func maxValue() -> Double
+    func defaultMin() -> Double
+    func defaultMax() -> Double
+}
